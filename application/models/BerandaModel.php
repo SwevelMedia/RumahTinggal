5dd574cd8817e32d8873001580d1aec5cd59d048
nsyifa <nasywa.syifa.azizah@mail.ugm.ac.id>
2024-03-17 17:45:04 +0700

    feat: queries to get jumlah unduh + jumlah pengunjung

--- application/models/BerandaModel.php
+++ application/models/BerandaModel.php
@@ -20,4 +20,14 @@ class BerandaModel extends CI_Model
 		$query = $this->db->get('testimoni'); // 'testimoni' adalah nama tabel dalam contoh ini
 		return $query->result(); // Mengembalikan hasil query dalam bentuk array objek
 	}
+
+	function getJumlahUnduh()
+	{
+		return $this->db->query('SELECT count(DISTINCT id_customer) as jumlah_unduh FROM pembelian WHERE STATUS = 1;');
+	}
+
+	function getJumlahPengunjung()
+	{
+		return $this->db->query('SELECT count(DISTINCT id_customer) as jumlah_pengunjung FROM customer WHERE expired = 0;');
+	}
 }
