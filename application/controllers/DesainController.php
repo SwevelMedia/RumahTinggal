76b7e3af3083e6066476686dcab2a9329d68e4b8
nsyifa <nasywa.syifa.azizah@mail.ugm.ac.id>
2024-03-17 17:43:41 +0700

    feat: filter function for koleksi desain

--- application/controllers/DesainController.php
+++ application/controllers/DesainController.php
@@ -50,30 +50,87 @@ class DesainController extends CI_Controller
     $data['title'] = 'Koleksi';
     $data['produk_favorit'] = $this->DesainModel->getKatalogProdukFavorit()->result();
     $data['koleksi_rumah'] = $this->DesainModel->getKatalogProdukNew()->result();
+    $data['range_panjang_lahan'] = $this->DesainModel->getRangePanjangLahan()->row();
+    $data['range_lebar_lahan'] = $this->DesainModel->getRangeLebarLahan()->row();
+    $data['gaya_desain'] = $this->DesainModel->getGayaDesain()->result();
+    $data['ruangan'] = $this->DesainModel->getRuang()->result();
 
     $sort = $this->input->get('sort');
 
     // Menangani logika pengurutan sesuai dengan opsi yang dipilih
-    switch ($sort) {
-      case 'populer':
-        $data['koleksi_rumah'] = $this->DesainModel->getKatalogProdukNew('populer')->result();
-        break;
-      case 'terbaru':
-        $data['koleksi_rumah'] = $this->DesainModel->getKatalogProdukNew('terbaru')->result();
-        break;
-      case 'harga_terendah':
-        $data['koleksi_rumah'] = $this->DesainModel->getKatalogProdukNew('harga_terendah')->result();
-        break;
-      case 'harga_tertinggi':
-        $data['koleksi_rumah'] = $this->DesainModel->getKatalogProdukNew('harga_tertinggi')->result();
-        break;
-      default:
-        // Default: Paling Sesuai atau urutan default lainnya
-        $data['koleksi_rumah'] = $this->DesainModel->getKatalogProdukNew()->result();
-        break;
-    }
+    // switch ($sort) {
+    //   case 'populer':
+    //     $data['koleksi_rumah'] = $this->DesainModel->getKatalogProdukNew('populer')->result();
+    //     break;
+    //   case 'terbaru':
+    //     $data['koleksi_rumah'] = $this->DesainModel->getKatalogProdukNew('terbaru')->result();
+    //     break;
+    //   case 'harga_terendah':
+    //     $data['koleksi_rumah'] = $this->DesainModel->getKatalogProdukNew('harga_terendah')->result();
+    //     break;
+    //   case 'harga_tertinggi':
+    //     $data['koleksi_rumah'] = $this->DesainModel->getKatalogProdukNew('harga_tertinggi')->result();
+    //     break;
+    //   default:
+    //     // Default: Paling Sesuai atau urutan default lainnya
+    //     $data['koleksi_rumah'] = $this->DesainModel->getKatalogProdukNew()->result();
+    //     break;
+    // }
+
+    $search = strtolower($this->input->get('search'));
+    $lantai = $this->input->get('lantai');
+    $min_panjang = $this->input->get('min_panjang');
+    $max_panjang = $this->input->get('max_panjang');
+    $min_lebar = $this->input->get('min_lebar');
+    $max_lebar = $this->input->get('max_lebar');
+    $min_biaya = $this->input->get('min_biaya');
+    $max_biaya = $this->input->get('max_biaya');
+    $kamar = $this->input->get('kamar');
+
+    $gaya = isset($_GET['gaya']) ? $_GET['gaya'] : '';
+    $ruang = isset($_GET['ruang']) ? $_GET['ruang'] : '';
+    // $gayas = explode(',', $gayaParam);
+
+    // $queryString = $_SERVER['QUERY_STRING']; // Get the raw query string
+    // $queryParams = explode('&', $queryString); // Split the query string into individual parameters
+
+    // $gayas = array(); // Initialize an array to store gaya values
+
+    // foreach ($queryParams as $param) {
+    //   // Split parameter into key and value
+    //   list($key, $value) = explode('=', $param);
+
+    //   // Decode URL-encoded values
+    //   $key = urldecode($key);
+    //   $value = urldecode($value);
+
+    //   // If the parameter is gaya, add its value to the $gayas array
+    //   if ($key === 'gaya') {
+    //     $gayas[] = $value;
+    //   }
+    // }
+
+    log_message('error',  'gayadesain' . var_export($gaya, true));
+
+    // Get the filtered records from the database
+    $data['koleksi_rumah'] = $this->DesainModel->getKatalogProdukNew($sort, $search, (int)$min_panjang, (int)$max_panjang, (int)$min_lebar, (int)$max_lebar, $lantai, $kamar, $gaya, $ruang, $min_biaya, $max_biaya)->result();
+
 
     $this->load->view('demo/layout/layout', $data);
+  }
+
+  public function filterProduk()
+  {
+    // $_SESSION['lantai'] = $_POST['lantai'];
+    log_message('error', 'masuk filterproduk' . var_export($_POST['lantai'], true));
+    $koleksi_rumah = $this->DesainModel->getKatalogProdukNew()->result();
+    $koleksi_rumah = array_filter($koleksi_rumah, 'filterByLantai');
+    return $koleksi_rumah;
+  }
+
+  function filterByLantai($rumah)
+  {
+    return $rumah->lantai == 2;
   }
 
   public function getKatalogProduk()
