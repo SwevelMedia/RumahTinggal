e92e2478ecaeb10af1a85fbf7f805e6c7eb99704
nsyifa <nasywa.syifa.azizah@mail.ugm.ac.id>
2024-03-17 17:45:33 +0700

    feat; queries to get additional data for koleksi

--- application/models/DesainModel.php
+++ application/models/DesainModel.php
@@ -16,6 +16,33 @@ class DesainModel extends CI_Model
 
 
 
+  function getJumlahRumah()
+  {
+    return $this->db->query("SELECT COUNT(*) AS jumlah_rumah FROM rumah;");
+  }
+
+  function getGayaDesain()
+  {
+    return $this->db->query("SELECT gaya_desain.id_gaya_desain, gaya_desain.gaya_desain FROM gaya_desain;");
+  }
+
+  function getRuang()
+  {
+    return $this->db->query("
+    SELECT DISTINCT ruang.id_ruang, ruang.ruang 
+    FROM ruang 
+    INNER JOIN ruang_rumah ON ruang.id_ruang = ruang_rumah.id_ruang");
+  }
+
+  function getRangePanjangLahan()
+  {
+    return $this->db->query("SELECT MAX(rumah.panjang_lahan) AS max_panjang_lahan, MIN(rumah.panjang_lahan) AS min_panjang_lahan FROM rumah;");
+  }
+
+  function getRangeLebarLahan()
+  {
+    return $this->db->query("SELECT MAX(rumah.lebar_lahan) AS max_lebar_lahan, MIN(rumah.lebar_lahan) AS min_lebar_lahan FROM rumah;");
+  }
 
 
   function getKatalogProdukPopular()
@@ -192,15 +219,23 @@ class DesainModel extends CI_Model
     return $this->db->query("SELECT a.kamar_tidur,a.toilet,a.id_rumah,a.foto, a.nama_rumah, a.id_arsitek, a.nama_arsitek,a.luas_lahan,a.lebar_lahan, a.panjang_lahan, a.luas_bangunan,a.lantai,a.tgl_buat,a.tgl_update,a.dilihat,a.suka,a.rating, a.harga_konstruksi,a.promo FROM (SELECT b.id_rumah, b.nama_rumah,b.kamar_tidur, b.toilet, b.id_arsitek,arsitek.nama_arsitek, ROUND(b.lebar_lahan) AS lebar_lahan, ROUND(b.panjang_lahan) AS panjang_lahan, ROUND(b.luas_lahan) AS luas_lahan ,ROUND(b.luas_bangunan) AS luas_bangunan,b.muka,b.wastafel,b.bathup,b.kloset_duduk,b.kloset_jongkok, b.foto,b.tgl_buat,b.tgl_update,b.suka,b.dilihat, max(b.lantai) AS lantai, round(AVG(IFNULL(ulasan.rating,0)),1) AS rating,harga_konstruksi,IFNULL(rumah_promo.promo,0) AS promo FROM (SELECT c.*, max(denah.lantai) AS lantai FROM (SELECT rumah.id_rumah, rumah.nama_rumah,x.kamar_tidur, x.toilet, rumah.id_arsitek, rumah.lebar_lahan, rumah.panjang_lahan, (rumah.lebar_lahan*rumah.panjang_lahan) AS luas_lahan,rumah.luas_bangunan,rumah.muka,rumah.wastafel,rumah.bathup,rumah.kloset_duduk,rumah.kloset_jongkok, rumah.foto,rumah.tgl_buat,rumah.tgl_update,rumah.suka,rumah.dilihat FROM (SELECT id_rumah, COUNT(CASE WHEN id_ruang = '7' THEN id_ruang END) kamar_tidur, COUNT(CASE WHEN id_ruang = '14' OR id_ruang='15' THEN id_ruang END) toilet FROM ruang_rumah GROUP BY id_rumah) x, rumah where x.id_rumah=rumah.id_rumah AND rumah.publish = 1) c, denah WHERE c.id_rumah = denah.id_rumah GROUP BY c.id_rumah, denah.lantai) b LEFT JOIN ulasan ON b.id_rumah = ulasan.id_rumah LEFT JOIN rumah_promo ON b.id_rumah = rumah_promo.id_rumah JOIN arsitek ON b.id_arsitek=arsitek.id_arsitek JOIN harga_konstruksi ON harga_konstruksi.id_rumah = b.id_rumah GROUP BY id_rumah) a join ruang_rumah ON a.id_rumah=ruang_rumah.id_rumah join gaya_desain_rumah ON a.id_rumah=gaya_desain_rumah.id_rumah join gaya_desain ON gaya_desain_rumah.id_gaya_desain=gaya_desain.id_gaya_desain $where GROUP BY id_rumah  $having $orderby");
   }
 
-  public function getKatalogProdukNew($sort = 'default', $search = '')
+  public function getKatalogProdukNew($sort = 'default', $search = '', $min_panjang = '', $max_panjang = '', $min_lebar = '', $max_lebar = '', $lantai = '', $kamar = '', $gaya = '', $ruang = '', $min_biaya = null, $max_biaya = null)
   {
     // Definisi kolom yang dapat diurutkan
     $sortableColumns = [
       'default' => 'a.id_rumah ASC',
       'populer' => 'a.dilihat DESC',
       'terbaru' => 'a.tgl_update DESC',
-      'harga_tertinggi' => 'a.luas_bangunan DESC',
-      'harga_terendah' => 'a.luas_bangunan ASC',
+      'harga_tertinggi' => "(CASE 
+                                WHEN a.lantai = '1' THEN 3500000 * a.luas_bangunan
+                                WHEN a.lantai = '2' THEN 4500000 * a.luas_bangunan
+                                ELSE 5500000 * a.luas_bangunan
+                            END) DESC",
+      'harga_terendah' => "(CASE 
+                                WHEN a.lantai = '1' THEN 3500000 * a.luas_bangunan
+                                WHEN a.lantai = '2' THEN 4500000 * a.luas_bangunan
+                                ELSE 5500000 * a.luas_bangunan
+                            END) ASC",
     ];
 
     // Validasi nilai sort agar tidak ada SQL injection
@@ -208,6 +243,48 @@ class DesainModel extends CI_Model
 
     // Tentukan kolom pengurutan berdasarkan nilai $sort
     $orderBy = $sortableColumns[$sort];
+
+    // Prepare WHERE clause
+    $whereConditions = [];
+    if (!empty($search)) {
+      $whereConditions[] = "LOWER(a.nama_rumah) LIKE LOWER('%{$search}%')";
+    }
+    if (!empty($lantai)) {
+      $whereConditions[] = "a.lantai = {$lantai}";
+    }
+    if (!empty($min_panjang) && !empty($max_panjang)) {
+      $whereConditions[] = "a.panjang_lahan BETWEEN {$min_panjang} AND {$max_panjang}";
+    }
+    if (!empty($min_lebar) && !empty($max_lebar)) {
+      $whereConditions[] = "a.lebar_lahan BETWEEN {$min_lebar} AND {$max_lebar}";
+    }
+    if (!empty($kamar)) {
+      $whereConditions[] = "a.kamar_tidur = {$kamar}";
+    }
+    if (!empty($gaya)) {
+      $whereConditions[] = "id_gaya_desain IN ({$gaya})";
+    }
+    if (!empty($ruang)) {
+      $whereConditions[] = "id_ruang IN ({$ruang})";
+    }
+    if ($min_biaya !== null) {
+      $whereConditions[] = "(
+            (a.lantai = '1' AND 3500000 * a.luas_bangunan >= {$min_biaya}) OR
+            (a.lantai = '2' AND 4500000 * a.luas_bangunan >= {$min_biaya}) OR
+            (a.lantai NOT IN ('1', '2') AND 5500000 * a.luas_bangunan >= {$min_biaya})
+        )";
+    }
+    if ($max_biaya !== null) {
+      $whereConditions[] = "(
+            (a.lantai = '1' AND 3500000 * a.luas_bangunan <= {$max_biaya}) OR
+            (a.lantai = '2' AND 4500000 * a.luas_bangunan <= {$max_biaya}) OR
+            (a.lantai NOT IN ('1', '2') AND 5500000 * a.luas_bangunan <= {$max_biaya})
+        )";
+    }
+
+    // Construct the WHERE clause
+    $whereClause = !empty($whereConditions) ? 'WHERE ' . implode(' AND ', $whereConditions) : '';
+
 
     // Query database dengan urutan yang sesuai
     $query = "
@@ -218,6 +295,8 @@ class DesainModel extends CI_Model
             a.id_rumah,
             a.foto,
             a.nama_rumah,
+            gaya_desain_rumah.id_gaya_desain AS id_gaya_design,
+            " . ($ruang ? "ruang_rumah.id_ruang AS id_ruang," : "") . "
             a.tgl_buat,
             a.id_arsitek,
             arsitek.nama_arsitek,
@@ -241,12 +320,17 @@ class DesainModel extends CI_Model
         ) a
         JOIN
             arsitek ON a.id_arsitek = arsitek.id_arsitek
+        LEFT JOIN
+            gaya_desain_rumah ON a.id_rumah = gaya_desain_rumah.id_rumah
+
+        " . ($ruang ? "LEFT JOIN ruang_rumah ON a.id_rumah = ruang_rumah.id_rumah" : "") . "
+        {$whereClause}
         GROUP BY
             a.id_rumah
         ORDER BY
             $orderBy
     ";
-
+    // log_message('error',  'query' . var_export($query, true));
     return $this->db->query($query);
   }
 
