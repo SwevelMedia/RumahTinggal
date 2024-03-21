123171e328b983daa5d83ce757f14729e7099d48
nsyifa <nasywa.syifa.azizah@mail.ugm.ac.id>
2024-03-21 09:56:41 +0700

    fix: composer autoload path

--- application/config/config.php
+++ application/config/config.php
@@ -149,8 +149,9 @@ $config['subclass_prefix'] = 'MY_';
 | Note: This will NOT disable or override the CodeIgniter-specific
 |	autoloading (application/config/autoload.php)
 */
-$config['composer_autoload'] = TRUE;
-$config['composer_autoload'] = 'google-api-php-client/src/Google/autoload.php';
+// $config['composer_autoload'] = TRUE;
+// $config['composer_autoload'] =  FCPATH . 'google-api-php-client/src/Google/autoload.php';
+$config['composer_autoload'] =  FCPATH . 'vendor/autoload.php';
 /*
 |--------------------------------------------------------------------------
 | Allowed URL Characters
@@ -213,7 +214,6 @@ $config['directory_trigger'] = 'd';
 |
 */
 $config['allow_get_array'] = TRUE;
-
 /*
 |--------------------------------------------------------------------------
 | Error Logging Threshold
