4a05f03bf4fffc592801d1ea420c53fe094b3a89
nsyifa <nasywa.syifa.azizah@mail.ugm.ac.id>
2024-03-22 14:02:37 +0700

    fix; mobile banner, input restrictions, article card size

--- application/views/demo/konten.php
+++ application/views/demo/konten.php
@@ -47,23 +47,36 @@
     }
 </style>
 
-<div class="owl-carousel owl-theme owl-carousel-eight d-lg-none" id="caraouselTesti">
+<!-- <div class="owl-carousel owl-theme owl-carousel-eight d-lg-none" id="caraouselTesti">
     <?php foreach ($produk_body as $item) { ?>
         <div style="background-image: url('<?= base_url('assets/img/desain/' . $item->foto) ?>'); object-fit:cover" class="card-img-top bg-card"></div>
     <?php } ?>
+</div> -->
+<div class="owl-carousel owl-theme owl-carousel-eight d-lg-none" id="caraouselTesti">
+    <?php foreach ($banner_mobile as $item) { ?>
+        <?php if ($item->id_rumah !== null) { ?>
+            <div style="background-image: url('<?= base_url('assets/demo/img/beranda/' . $item->nama_rumah) . '.svg' ?>'); object-fit:cover" class="card-img-top bg-card" href="<?= base_url('detail_koleksi/' . $item->id_rumah) ?>"></div>
+        <?php } ?>
+    <?php } ?>
 </div>
-<div class="py-5 bg-header-web-home overflow-hidden" style="background-image:radial-gradient(circle at -90%, white 50%, #53b4fc 100%);">
-    <div class="container px-4 home-banner-left px-lg-0">
-        <div class="row home-banner-left">
+<div class="pt-md-0 py-md-5 bg-header-web-home overflow-hidden" style="background-image:radial-gradient(circle at -90%, white 50%, #53b4fc 100%);">
+    <div class="container px-2 home-banner-left px-lg-0">
+        <div class="row m-0 p-0 home-banner-left">
             <!-- Bagian Kiri (Teks) -->
             <div class="col-lg-6 ms-lg-4 d-flex flex-column h-100 justify-content-center home-banner-left">
-                <h1 class="fw-bold mb-3 text-primary display-4 d-none d-md-block">Dapatkan Desain</h1>
-                <h1 class="fw-bold mb-3 text-primary display-4 d-none d-md-block">Hunian Idaman Anda!</h1>
-                <h1 class="fw-bold mb-3 text-primary display-5 d-block d-md-none">Dapatkan Desain</h1>
-                <h1 class="fw-bold mb-3 text-primary display-5 d-block d-md-none">Hunian Idaman Anda!</h1>
-                <p class="mb-3">Bangun rumah lebih mudah bersama RumahTinggal.id</p>
+                <div class="d-none d-md-block">
+                    <h1 class="fw-bold mb-1 text-primary display-4">Dapatkan Desain</h1>
+                    <h1 class="fw-bold mb-3 text-primary display-4">Hunian Idaman Anda!</h1>
+                </div>
+                <div class="d-block d-md-none">
+                    <h1 class="fw-bold mb-1 text-primary display-5">Dapatkan Desain</h1>
+                    <h1 class="fw-bold mb-2 text-primary display-5">Hunian Idaman Anda!</h1>
+                </div>
+
+                <p class="mb-4 d-none d-md-block fs-5">Bangun rumah lebih mudah bersama RumahTinggal.id</p>
+                <small class="mb-3 d-md-none">Bangun rumah lebih mudah bersama RumahTinggal.id</small>
                 <div class="row">
-                    <div class="col-lg-9 mb-5">
+                    <div class="col-lg-9 mb-md-5 mb-4">
                         <form onsubmit="event.preventDefault(); clickSearch(document.getElementById('textbox-cari-desain').value);">
                             <div class="input-group mb-3">
                                 <input type="text" class="form-control" placeholder="Cari desain" id="textbox-cari-desain" />
@@ -86,34 +99,61 @@
                                 <div class="row">
                                     <div class="col-4">
                                         <div class="d-flex flex-column flex-lg-row align-items-center justify-content-center gap-2">
-                                            <div>
+                                            <div class="d-none d-md-block">
                                                 <img src="<?php echo base_url('assets/gif/home.png'); ?>" width="50" height="50">
                                             </div>
-                                            <div class="text-center text-lg-start">
+                                            <div class="d-block d-md-none">
+                                                <img src="<?php echo base_url('assets/gif/home.png'); ?>" width="35" height="35">
+                                            </div>
+                                            <div class="text-center text-lg-start d-none d-md-block">
                                                 <div class="fs-5 fw-semibold"><?php echo floor($jumlah_rumah / 10) * 10 . '+'; ?>
                                                 </div>
                                                 <small>Koleksi Desain</small>
                                             </div>
+                                            <div class="text-center text-lg-start d-block d-md-none" style="line-height:1.2">
+                                                <div class="mb-1 fs-6 fw-semibold"><?php echo floor($jumlah_rumah / 10) * 10 . '+'; ?>
+                                                </div>
+                                                <small>Koleksi Desain</small>
+                                            </div>
+
                                         </div>
                                     </div>
                                     <div class="col-4">
                                         <div class="d-flex flex-column flex-lg-row align-items-center justify-content-center gap-2">
-                                            <div>
+                                            <div class="d-none d-md-block">
                                                 <img src="<?php echo base_url('assets/gif/folder-download.png'); ?>" width="50" height="50">
                                             </div>
-                                            <div class="text-center text-lg-start">
-                                                <div class="fs-5 fw-semibold"><?php echo floor($jumlah_unduh / 100) * 100 . '+'; ?></div>
+                                            <div class="d-block d-md-none">
+                                                <img src="<?php echo base_url('assets/gif/folder-download.png'); ?>" width="35" height="35">
+                                            </div>
+                                            <div class="text-center text-lg-start d-none d-md-block">
+                                                <div class="fs-5 fw-semibold"><?php echo floor($jumlah_unduh / 100) * 100 . '+'; ?>
+                                                </div>
+                                                <small>Desain Diunduh</small>
+                                            </div>
+                                            <div class="text-center text-lg-start d-block d-md-none" style="line-height:1.2">
+                                                <div class="mb-1 fs-6 fw-semibold"><?php echo floor($jumlah_unduh / 100) * 100 . '+'; ?>
+                                                </div>
                                                 <small>Desain Diunduh</small>
                                             </div>
                                         </div>
                                     </div>
                                     <div class="col-4">
                                         <div class="d-flex flex-column flex-lg-row align-items-center justify-content-center gap-2">
-                                            <div>
+                                            <div class="d-none d-md-block">
                                                 <img src="<?php echo base_url('assets/gif/users.png'); ?>" width="50" height="50">
                                             </div>
-                                            <div class="text-center text-lg-start">
-                                                <div class="fs-5 fw-semibold"><?php echo floor($jumlah_pengunjung / 100) * 100 . '+'; ?></div>
+                                            <div class="d-block d-md-none">
+                                                <img src="<?php echo base_url('assets/gif/users.png'); ?>" width="35" height="35">
+                                            </div>
+                                            <div class="text-center text-lg-start d-none d-md-block">
+                                                <div class="fs-5 fw-semibold"><?php echo floor($jumlah_pengunjung / 100) * 100 . '+'; ?>
+                                                </div>
+                                                <small>Pengunjung Aktif</small>
+                                            </div>
+                                            <div class="text-center text-lg-start d-block d-md-none" style="line-height:1.2">
+                                                <div class="mb-1 fs-6 fw-semibold"><?php echo floor($jumlah_pengunjung / 100) * 100 . '+'; ?>
+                                                </div>
                                                 <small>Pengunjung Aktif</small>
                                             </div>
                                         </div>
@@ -139,7 +179,7 @@
 <!-- Head end -->
 
 <!--Temukan Desain start-->
-<div class="py-5">
+<div class="py-5 px-1 px-lg-0">
     <div class="container">
         <form class="row gx-lg-5" id="form-impian">
             <div class="col-12 mb-3">
@@ -187,7 +227,7 @@
                 <div class="row">
                     <div class="form-group mb-3 col-6 col-lg-12">
                         <label for="jumlah_lantai" class="form-label fw-semibold">Jumlah Lantai</label>
-                        <input type="number" class="form-control" id="jumlah_lantai" name="lantai" min="1" placeholder="Jumlah Lantai" />
+                        <input type="number" class="form-control numeric-input" id="jumlah_lantai" name="lantai" min="1" placeholder="Jumlah Lantai" />
                     </div>
                     <div class="form-group mb-3 col-6 col-lg-12">
                         <label for="jumlah_lantai" class="form-label fw-semibold">Gaya Desain</label>
@@ -206,7 +246,7 @@
         </form>
     </div>
 </div>
-<div class="py-5 bg-light">
+<div class="pt-5 bg-light w-100 overflow-hidden">
     <div class="container">
         <div class="d-flex justify-content-between flex-column flex-lg-row align-items-center mb-3">
             <h1 class="fw-semibold text-center ms-3">Rekomendasi Desain</h1>
@@ -311,8 +351,8 @@
             </div>
 
 
-            <div class="position-absolute top-50 start-100 translate-middle d-lg-block" style="z-index: 1000">
-                <button class="btn btn-white border-0 bg-white shadow-sm rounded-circle me-3" id="nextBtnOne"><i class="fas fa-arrow-right fs-7"></i></button>
+            <div class="position-absolute top-50 start-100 translate-middle d-lg-block" style="z-index: 1000; width:min-content;">
+                <button class="btn btn-white border-0 bg-white shadow-sm rounded-circle" id="nextBtnOne"><i class="fas fa-arrow-right fs-7"></i></button>
             </div>
             <div class="position-absolute top-50 start-0 translate-middle" style="z-index: 1000">
                 <button class="btn btn-white border-0 bg-white shadow-sm rounded-circle" id="prevBtnOne"><i class="fas fa-arrow-left"></i></button>
@@ -325,7 +365,7 @@
         </div>
     </div>
 </div>
-<div class="py-5 bg-light" style="position: relative;">
+<div class="w-100 overflow-hidden py-5 bg-light" style="position: relative;">
     <div class="container">
         <div class="d-flex justify-content-between flex-column flex-lg-row align-items-center mb-3">
             <h1 class="fw-semibold text-center ms-3">Desain Terbaru</h1>
@@ -426,7 +466,7 @@
                 <?php } ?>
             </div>
             <div class="position-absolute top-50 start-100 translate-middle d-lg-block" style="z-index: 1000">
-                <button class="btn btn-white border-0 bg-white shadow-sm rounded-circle me-3" id="nextBtnTwo"><i class="fas fa-arrow-right fs-7"></i></button>
+                <button class="btn btn-white border-0 bg-white shadow-sm rounded-circle" id="nextBtnTwo"><i class="fas fa-arrow-right fs-7"></i></button>
             </div>
             <div class="position-absolute top-50 start-0 translate-middle" style="z-index: 1000">
                 <button class="btn btn-white border-0 bg-white shadow-sm rounded-circle" id="prevBtnTwo"><i class="fas fa-arrow-left"></i></button>
@@ -439,7 +479,7 @@
         </div>
     </div>
 </div>
-<div class="py-5">
+<div class="py-5 w-100 overflow-hidden">
     <div class="container">
         <div class="row">
             <div class="col-12 mb-3">
@@ -506,7 +546,7 @@
 
                     </div>
                     <div class="position-absolute top-50 start-100 translate-middle" style="z-index: 1000">
-                        <button class="btn btn-white border-0 bg-white shadow-sm rounded-circle me-3" id="nextBtnThree"><i class="fas fa-arrow-right fs-7"></i></button>
+                        <button class="btn btn-white border-0 bg-white shadow-sm rounded-circle" id="nextBtnThree"><i class="fas fa-arrow-right fs-7"></i></button>
                     </div>
                     <div class="position-absolute top-50 start-0 translate-middle" style="z-index: 1000">
                         <button class="btn btn-white border-0 bg-white shadow-sm rounded-circle" id="prevBtnThree"><i class="fas fa-arrow-left"></i></button>
@@ -533,7 +573,7 @@
         </div>
     </div>
 </div>
-<div class="py-4 bg-light">
+<div class="pt-5 bg-light">
     <div class="container">
         <div class="row">
             <div class="col-lg-4 mb-3">
@@ -564,10 +604,16 @@
                                 $deskripsi_meta = $item->deskripsi_meta;
                                 $isi = (strlen($deskripsi_meta) > 50) ? substr($deskripsi_meta, 0, 75) . '...' : $deskripsi_meta;
                         ?>
-                                <div class="card mb-3 shadow-sm" style="max-width: 600px">
+                                <div class="card artikel-card mb-3 shadow-sm" style="max-width: 600px">
                                     <div class="row g-0">
                                         <div class="col-md-4 px-2 py-2">
-                                            <div style="background-image: url('<?= base_url('assets/img/artikel/' . $item->foto_cover) ?>')" class="card-img-artikel bg-card"></div>
+                                            <div class="bg-card d-md-none">
+                                                <img class="image" src="<?= base_url('assets/img/artikel/' . $item->foto_cover) ?>" style="object-fit: cover; width: 100%; height: 100%;" onload='updateArticleHeight()'>
+                                            </div>
+                                            <div class="bg-card d-none d-md-block">
+                                                <img class="image" src="<?= base_url('assets/img/artikel/' . $item->foto_cover) ?>" style="object-fit: cover; width: 100%; height: 100%;">
+                                            </div>
+
                                         </div>
                                         <div class="col-md-8">
                                             <div class="card-body" id="myCardBody">
@@ -613,7 +659,7 @@
             </div>
         </div>
         <div class="d-block d-lg-none text-center mt-3">
-            <button class="btn btn-primary" type="button" id="button-addon2" onclick="katalogMaterial()">Lihat Semua<i class=" ms-2 bi bi-arrow-right"></i></button>
+            <button class="btn btn-primary" type="button" id="button-addon2" onclick="katalogMaterial()">Lihat Semua<i class="ms-2 fas fa-arrow-right"></i></button>
         </div>
 
     </div>
@@ -633,17 +679,18 @@
         playIconContainer.style.display = 'none';
     });
 
-    // $(document).ready(function() {
-    //     $('#button-addon2').on('click', function() {
-    //         var cari = $('#cari-desain-input').val(); // Ambil nilai dari input pencarian
+    //function to prevent non-numeric inputs in numeric inputs
+    $(document).ready(function() {
+        function restrictToNumeric(event) {
+            var inputValue = $(this).val();
 
-    //         // Buat URL dengan nilai pencarian
-    //         var url = "<?= base_url('demo/koleksi#') ?>" + cari;
+            var numericValue = inputValue.replace(/\D/g, '');
 
-    //         // Arahkan pengguna ke URL yang baru
-    //         window.location.href = url;
-    //     });
-    // });
+            $(this).val(numericValue);
+        }
+
+        $('.numeric-input').on("input", restrictToNumeric);
+    });
 
     $(".js-range-slider-panjang").ionRangeSlider({
         skin: "round",
@@ -889,7 +936,7 @@
                                 <hr/>
                                 <h6 class="harga_material mb-0"><strong>${formatRupiah(harga)}</strong> /buah</h6>
                                 <div class="mt-3">
-                                    <button class="btn btn-success w-100" type="button" id="button-addon2" onclick="detailProduk(${data[i].id_produk})"><i class="me-2 fa-solid fa-cart-shopping"></i> Beli Sekarang</button>
+                                    <button class="btn btn-primary w-100" type="button" id="button-addon2" onclick="detailProduk(${data[i].id_produk})"><i class="fa-solid fa-file-import me-2"></i> Lihat Detail</button>
                                 </div>
                             </div>
                         </div>
@@ -987,28 +1034,32 @@
         });
     }
 
-    // to make cards in design carousels the same height
-    // document.addEventListener("DOMContentLoaded", function() {
-    //     var cards2 = document.querySelectorAll('.owl-carousel-two .card');
+    $(window).resize(updateArticleHeight);
+
+
+    function updateArticleHeight() {
+        var cards2 = document.querySelectorAll('.owl-carousel-five .artikel-card');
+        var maxHeight2 = 0;
+        cards2.forEach(function(card2) {
+            maxHeight2 = Math.max(maxHeight2, card2.offsetHeight);
+        });
+        cards2.forEach(function(card2) {
+            card2.style.height = maxHeight2 + 'px';
+        });
+
+    }
+
+    // $(document).ready(function() {
+    //     var cards2 = document.querySelectorAll('.artikel-card');
 
     //     var maxHeight2 = 0;
     //     cards2.forEach(function(card2) {
     //         maxHeight2 = Math.max(maxHeight2, card2.offsetHeight);
     //     });
-    //     cards2.forEach(function(card2) {
-    //         card2.style.height = maxHeight2 + 'px';
-    //     });
-    // });
+    //     console.log(cards2.offsetHeight)
 
-    // document.addEventListener("DOMContentLoaded", function() {
-    //     var cards2 = document.querySelectorAll('.owl-carousel-one .card');
-
-    //     var maxHeight2 = 0;
-    //     cards2.forEach(function(card2) {
-    //         maxHeight2 = Math.max(maxHeight2, card2.offsetHeight);
-    //     });
-    //     cards2.forEach(function(card2) {
-    //         card2.style.height = maxHeight2 + 'px';
-    //     });
-    // });
+    //     // cards2.forEach(function(card2) {
+    //     //     card2.style.height = maxHeight2 + 'px';
+    //     // });
+    // })
 </script>
