f00001b3c1adec599a40f1ea8475e5701114b1e5
nsyifa <nasywa.syifa.azizah@mail.ugm.ac.id>
2024-03-27 09:08:08 +0700

    -- feat: add kebijakan privasi dan syarat ketentuan pages

--- application/controllers/LayananController.php
+++ application/controllers/LayananController.php
@@ -1,26 +1,33 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 header("Access-Control-Allow-Origin: *");
 
-class LayananController extends CI_Controller {
+class LayananController extends CI_Controller
+{
     public function __construct()
     {
         parent::__construct();
-         // $this->load->model("DesainModel");
+        // $this->load->model("DesainModel");
     }
-    public function kebijakan_privasi() {
-        $data['halaman'] = 'layanan/kebijakan_privasi';
-        $this->load->view('layout', $data);
+    public function kebijakan_privasi()
+    {
+        $data['title'] = 'Kebijakan Privasi';
+        $data['halaman'] = 'demo/kebijakan_privasi';
+        $this->load->view('demo/layout/layout', $data);
     }
-    public function syarat_ketentuan() {
-        $data['halaman'] = 'layanan/syarat_ketentuan';
-        $this->load->view('layout', $data);
+    public function syarat_ketentuan()
+    {
+        $data['title'] = 'Syarat dan Ketentuan';
+        $data['halaman'] = 'demo/syarat_ketentuan';
+        $this->load->view('demo/layout/layout', $data);
     }
-    public function cara_kerja() {
+    public function cara_kerja()
+    {
         $data['halaman'] = 'layanan/cara_kerja';
         $this->load->view('layout', $data);
     }
-    public function faq() {
+    public function faq()
+    {
         $data['halaman'] = 'layanan/faq';
         $this->load->view('layout', $data);
     }
