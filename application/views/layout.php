fdc07d53a36babdf3ce23188d3fbf11f5509fead
nsyifa <nasywa.syifa.azizah@mail.ugm.ac.id>
2024-04-18 13:53:49 +0700

    fix; modified layout for old pages

--- application/views/layout.php
+++ application/views/layout.php
@@ -1,13 +1,21 @@
 <!DOCTYPE html>
 
 <html lang="en">
-    <?php $this->load->view('layout/header') ?>
-    <body>
-        <?php $this->load->view('layout/topbar') ?>
-        <?php $this->load->view($halaman) ?>
-        <?php $this->load->view('layout/footer') ?>
-        <?php $this->load->view('layout/script') ?>
-    </body>
+
+
+<?php $this->load->view('layout/header') ?>
+
+<body>
+    <?php
+    // Load common data directly in the layout file
+    $data['jumlah_rumah'] = $this->DesainModel->getJumlahRumah()->row()->jumlah_rumah;
+    $data['google_client_id'] = '619189282883-54euurh55b0od41supra7n7eoksj2jbu.apps.googleusercontent.com';
+    ?>
+    <?php $this->load->view($halaman) ?>
+    <?php $this->load->view('demo/layout/footer') ?>
+    <?php $this->load->view('layout/script') ?>
+</body>
+
 </html>
 
 <script>
@@ -16,5 +24,29 @@
         preventDuplicates: true,
         timeOut: 5000
     }
-    
+
+    function number_format(number, decimals, dec_point, thousands_point) {
+        if (number == null || !isFinite(number)) {
+            throw new TypeError("number is not valid");
+        }
+        if (!decimals) {
+            let len = number.toString().split('.').length;
+            decimals = len > 1 ? len : 0;
+        }
+        if (!dec_point) {
+            dec_point = '.';
+        }
+        if (!thousands_point) {
+            thousands_point = ',';
+        }
+
+        number = parseFloat(number).toFixed(decimals);
+        number = number.replace(".", dec_point);
+        var splitNum = number.split(dec_point);
+        splitNum[0] = splitNum[0].replace(/\B(?=(\d{3})+(?!\d))/g, thousands_point);
+        //number = splitNum.join(dec_point);
+        number = splitNum[0];
+
+        return number;
+    }
 </script>
